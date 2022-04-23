124ecc8be5029c22e3717ea07ba567955affdc26
ErickJHernandezM <99061757+ErickJHernandezM@users.noreply.github.com>
2022-04-23 15:22:19 -0500

    Delete src/practicaMultiverse directory

--- src/practicaMultiverse/SNoir.java
+++
@@ -1,7 +0,0 @@
-package practicaMultiverse;
-
-public class SNoir extends Multiverso implements SpiderNoir {
-    public SNoir(String nombre,String alias, int tierra){
-        super(nombre,alias,tierra);
-    }
-}
--- src/practicaMultiverse/Spiderh.java
+++
@@ -1,7 +0,0 @@
-package practicaMultiverse;
-
-public class Spiderh extends Multiverso implements Spiderham {
-    public Spiderh(String nombre,String alias, int tierra){
-        super(nombre,alias,tierra);
-    }
-}
